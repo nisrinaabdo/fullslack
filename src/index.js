b41dbeecee9cdce30e5a55e7ccbf3123ab0df46a
Cédric Farcy <cedricfarcy31@gmail.com>
2018-02-13 09:53:41 +0100

    Fixes #3 : initialize firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+import { firebase } from 'firebase';
 
 import { reducer as authReducer } from './modules/auth';
 import { reducer as mainReducer } from './modules/main';
@@ -12,6 +13,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyBEcsJrTE3zQX9KWI4ludSzKUwm7X0n904",
+    authDomain: "dafs-59108.firebaseapp.com",
+    databaseURL: "https://dafs-59108.firebaseio.com",
+    projectId: "dafs-59108",
+    storageBucket: "dafs-59108.appspot.com",
+    messagingSenderId: "898840147930"
+  });
 const appReducer = combineReducers({
     auth: authReducer,
     main: mainReducer,
