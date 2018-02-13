0a252138e6f337fc2a2bf8d5304ed46d8cca6024
Florent Gornes <gornes.florent@gmail.com>
2018-02-13 09:55:02 +0100

    Fixes #5 : add redux devtools

--- src/index.js
+++ src/index.js
@@ -19,12 +19,15 @@ const appReducer = combineReducers({
     rooms: roomsReducer
 })
 
-const store = createStore(appReducer);
+const store = createStore(
+    appReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
-    </Provider>, 
+    </Provider>,
     document.getElementById('root')
 );
 registerServiceWorker();
