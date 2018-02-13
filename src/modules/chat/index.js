bc8425e2fe2d93ee2984167c36d0ae3487fdf8c6
mluzarre <manuel.luzarreta@etu.ensat.fr>
2018-02-13 13:51:31 +0100

    resolve conflicts

--- src/modules/chat/index.js
+++ src/modules/chat/index.js
@@ -5,16 +5,23 @@ import {
   Link
 } from 'react-router-dom'
 
+<<<<<<< HEAD
 const message = 
+=======
+>>>>>>> 3f640b8aae04532a6fc83c84027379cb2f5f6a89
 
 const messageList = () => (
   
 )
 
 
+<<<<<<< HEAD
 const chatBox = () => (
 
 )
+=======
+const chatBox = 
+>>>>>>> 3f640b8aae04532a6fc83c84027379cb2f5f6a89
 
 
 export const reducer = (state = {} , action) => state;
