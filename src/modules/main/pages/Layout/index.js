d1fdda51bb36378deff94cf1cbdb62a48d55cc52
Frederik Eychenié <frederik.eychenie@sigfox.com>
2018-02-13 11:53:56 +0100

    add router

--- src/modules/main/pages/Layout/index.js
+++ src/modules/main/pages/Layout/index.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
+import { connect  } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './layout.css';
 
 class Layout extends Component {
     render(){
         return (
-            <div className="app-layout">
-                <header className="app-header">
-                    <h1 className="app-title">Fullslack</h1>
-                </header>
-                <aside className="app-side">
-
-                </aside>
-                <section className="app-main">
-                    
-                </section>
-            </div>
+            <Router>
+                <div className="app-layout">
+                    <header className="app-header">
+                        <h1 className="app-title">Fullslack</h1>
+                    </header>
+                    <aside className="app-side">
+                        { /* profile header & rooms list */ }
+                    </aside>
+                    <section className="app-main">
+                        { /* routes, ex: current chat window, other pages */ }
+                    </section>
+                </div>
+            </Router>
         );
     } 
 }
 
-export default Layout;
+export default connect()(Layout);
