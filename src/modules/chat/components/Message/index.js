85892e9e5183baa1343d334f29b2572ee0f2352d
mluzarre <manuel.luzarreta@etu.ensat.fr>
2018-02-13 16:47:17 +0100

    Correct bug into MessageBox

--- src/modules/chat/components/Message/index.js
+++ src/modules/chat/components/Message/index.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 
 class Message extends Component {
   render() {
-    const { user, datetime, body } = this.props;
+    const { user, datetime, messageBody } = this.props;
     return (
       <div className="messageWrapper">
         <h4> { user } </h4> 
         <p> { datetime } </p>
-        <p> { body} </p>
+        <p> { messageBody } </p>
       </div>
     )
   }
