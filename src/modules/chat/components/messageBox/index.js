4b8c6a021ba149a69d870b83ca69ef721a5ecfc3
mluzarre <manuel.luzarreta@etu.ensat.fr>
2018-02-13 15:16:31 +0100

    Finish messageBox component

--- src/modules/chat/components/messageBox/index.js
+++ src/modules/chat/components/messageBox/index.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { submitMessage } from '../../index';
 
 class messageBox extends Component {
-  submitMessage = (e) => {
-    e.preventDefault()
 
-
+  state = {
+    user: 'toto',
+    messageBody: '',
+    datetime:'',
+    room: 'ROOM 1'
+  }
+  
+  onSubmitMessage = (e) => {
+    e.preventDefault();
+    this.state.datetime = new Date.now()
+    this.props.dispatch(submitMessage(this.state))
   }
   
   render() {
+    const { messageBody } = this.state;
     return (
       <div className="messageBox">
         <input
-          id='message-box-body'
+          type='text'
           value={ messageBody }
           placeholder='Put your text here'
         />
         <button
           id='mesage-box-button'
           type='submit'
-          onclick={ this.submitMessage}
+          onclick={ this.onSubmitMessage }
           > Send ! </button>
       </div>
     )
