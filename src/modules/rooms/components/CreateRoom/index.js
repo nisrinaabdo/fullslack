510851ebe3d808c73562662b0521859a80629e6f
Sairally ehsan <ehsan.sairally@gmail.com>
2018-02-13 16:33:08 +0100

    ajout formulaire creation

--- src/modules/rooms/components/CreateRoom/index.js
+++ src/modules/rooms/components/CreateRoom/index.js
@@ -3,10 +3,46 @@ import React, { Component } from 'react'
 import './createRoom.css'
 
 class CreateRoom extends Component {
+  constructor (props){
+    super(props)
+    this.state={
+      formActive: false
+    }
+
+    this.showForm = this.showForm.bind(this)
+    this.showAddButton = this.showAddButton.bind(this)
+  }
+
+  displayAddButton = () => {
+    return (
+      <button onClick={this.showForm}>Add</button>
+    )
+  }
+
+  showForm = (event) => {
+    event.preventDefault()
+    this.setState({formActive: true})
+  }
+
+  displayForm = () => {
+    return (
+      <form onSubmit={this.handleCreation}>
+        <input type="text" name="name" />
+        <input type="submit" value="OK" />
+        <button onClick={this.showAddButton}>CANCEL</button>
+      </form>
+    )
+  }
+
+  showAddButton = (event) => {
+    event.preventDefault()
+    this.setState({formActive: false})
+  }
+
   render () {
     return (
       <div className='createRoomWrapper'>
-        <button>Add</button>
+        {this.state.formActive ? this.displayForm() : this.displayAddButton()}
       </div>
     )
   }
