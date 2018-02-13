8b83d82f15e09d6dc8ebe1a551c74a2c4e37fd75
Cédric Farcy <cedricfarcy31@gmail.com>
2018-02-13 14:57:47 +0100

    remove <br> im Message/index.js and add Maj in dir

--- src/modules/chat/components/message/index.js
+++ src/modules/chat/components/message/index.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 
-class message extends Component {
+class Message extends Component {
   render() {
     const { user, datetime, body } = this.props;
     return (
       <div className="messageWrapper">
-        <h4> { user } </h4> <p> { datetime } </p><br>
+        <h4> { user } </h4> 
+        <p> { datetime } </p>
         <p> { body} </p>
       </div>
     )
   }
 }
 
-export default message;
+export default Message;
