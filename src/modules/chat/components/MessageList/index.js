e30bfca2db8a47c0675604ec43f0bb1626bb4db4
Cédric Farcy <cedricfarcy31@gmail.com>
2018-02-13 16:29:20 +0100

    fixe messageList

--- src/modules/chat/components/MessageList/index.js
+++ src/modules/chat/components/MessageList/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { Message } from '../message';
+import  Message  from '../Message';
 
 class MessageList extends Component {
   render() {
     const { messages } = this.props;
     return (
     <div>
-      { messages.map((msg) => <Msg {...msg}/> )}
+      { messages.map((msg) => <Message {...msg}/> )}
     </div>
     )
   }
