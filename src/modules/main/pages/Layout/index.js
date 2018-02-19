d736ed6126e95ad34403cbacea42bb0267c985fa
mluzarre <manuel.luzarreta@etu.ensat.fr>
2018-02-19 10:03:06 +0100

    Minor change due to error

--- src/modules/main/pages/Layout/index.js
+++ src/modules/main/pages/Layout/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-<<<<<<< HEAD
 
 import Login from '../../../auth/components/Login'
 import Logout from '../../../auth/components/Logout'
@@ -38,30 +37,6 @@ class Layout extends Component {
       </Router>
     );
   }
-=======
-import ChatList from '../../../chat/pages/ChatList'
-import './layout.css';
-
-class Layout extends Component {
-    render(){
-        return (
-            <Router>
-                <div className="app-layout">
-                    <header className="app-header">
-                        <h1 className="app-title">Fullslack</h1>
-                    </header>
-                    <aside className="app-side">
-
-                    </aside>
-                    <section className="app-main">
-                        <ChatList/>
-                        
-                    </section>
-                </div>
-            </Router>
-        );
-    } 
->>>>>>> e09e74c51de52fb939ee4f8eb5fb07300c995f0b
 }
 
 export default connect(
