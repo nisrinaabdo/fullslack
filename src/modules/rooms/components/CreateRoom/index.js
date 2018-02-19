47ef4c160fc21fbd722bdea37a8642be556a9e2f
Sairally ehsan <ehsan.sairally@gmail.com>
2018-02-19 10:36:30 +0100

    Utilisation du "slug" uniquement à la validation du formulaire

--- src/modules/rooms/components/CreateRoom/index.js
+++ src/modules/rooms/components/CreateRoom/index.js
@@ -8,14 +8,12 @@ class CreateRoom extends Component {
   constructor (props){
     super(props)
     this.state={
-      formActive: false,
-      roomName: ''
+      formActive: false
     }
 
     this.showForm = this.showForm.bind(this)
     this.showAddButton = this.showAddButton.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleRoomNameChange = this.handleRoomNameChange.bind(this)
   }
 
   displayAddButton = () => {
@@ -37,7 +35,7 @@ class CreateRoom extends Component {
       <div id="addFormWrapper">
         <span>Add new chat-room:</span>
         <form onSubmit={this.handleSubmit}>
-          <input type="text" name="name" placeholder="Name" onChange={ this.handleRoomNameChange } value={ roomName } required/>
+          <input type="text" name="name" placeholder="Name" required/>
           <input type="text" name="description" placeholder="Description"/>
           <input type="submit" className="btn btn-ok" value="OK" />
           <button onClick={this.showAddButton} className="btn btn-cancel">CANCEL</button>
@@ -47,54 +45,34 @@ class CreateRoom extends Component {
   }
 
   /*
-    Process room name while changing
-  */
-  handleRoomNameChange = (event) => {
-    let currentName = event.target.value
-
-    //Replace last char if space as hyphen
-    if (currentName !== '') {
-      const lastChar = currentName.substr(currentName.length - 1)
-      if (lastChar === ' ') {
-        currentName = currentName.substr(0, currentName.length - 1) + '-'
-      }
-    }
-
-    //Transform to "slug" the current room name
-    const newName = slugify(currentName, {
-      replacement: '-',
-      remove: /[$*_+~.()'"!;?{}/:@]/g,
-      lower: true
-    })
-
-    //Re-render the componant due to local changes
-    this.setState({ roomName: newName})
-  }
-
-  /*
     Handle form submitted data
   */
   handleSubmit = (event) => {
-    //Process room name value (local state)
-    let roomName = this.state.roomName.trim()
-    const lastChar = roomName.substr(roomName.length - 1)
-    const proceedRoomName = (lastChar === '-') ? roomName.substr(0, roomName.length - 1) : roomName
-
     //Prevent default behaviour
     event.preventDefault()
 
     //Send data the reducer
     if (event) {
       const target = event.target
+
+      //Process room name value
+      const roomId = slugify(target.name.value, {
+        replacement: '-',
+        remove: /[$*_+~.()'"!;?{}/:@]/g,
+        lower: true
+      })
+
       const newRoom = {
-        name: proceedRoomName,
+        id: roomId,
+        name: target.name.value,
         description: target.description.value.trim()
       }
+
       //Use callback passed as parameters with room object
       this.props.creationCbk(newRoom)
 
       //Seg local stat to trigger local rendering
-      this.setState({formActive: false, roomName: ''})
+      this.setState({formActive: false})
     }
   }
 
