e052a31a7314d2143229a7860d0f33d64e00dc4f
mluzarre <manuel.luzarreta@etu.ensat.fr>
2018-02-19 17:23:06 +0100

    Display messages from firebase. Fully functionnal !

--- src/modules/chat/components/MessageList/index.js
+++ src/modules/chat/components/MessageList/index.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { fetchMessages } from '../../index';
+import { fetchFromFirebase } from '../../index';
 import  Message  from '../Message';
 
 class MessageList extends Component {
 
   componentDidMount() {
-    const { dispatch } = this.props;
-    const idRoom = this.props.idRoom;
-    dispatch(fetchMessages(idRoom))
-    console.log(`idRoom : ${idRoom}`)
-
+    const { dispatch, idRoom } = this.props;
+    dispatch(fetchFromFirebase(idRoom))
   }
 
   render() {
     const { messages } = this.props;
     return (
     <div>
-      { messages.map((msg) => <Message {...msg}/> )}
+      { messages.map((msg, index) => <Message key={ index } {...msg}/> )}
     </div>
     )
   }
